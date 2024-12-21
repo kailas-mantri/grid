3276d1219898c0deab5a577f37107cd47f04d125
kailas-mantri <kailasmantri@gmail.com>
2024-12-22 05:14:57 +0530

    check

--- lib/main.dart
+++ lib/main.dart
@@ -44,7 +44,7 @@ class SplashState extends State<SplashScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       backgroundColor: Colors.blue,
       body: Center(
         child: Padding(
